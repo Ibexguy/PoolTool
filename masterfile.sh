0a8ece8fe821445ddcf51598865a5326f1e9248f
Mathieu Robin <mathieu.robin@uzh.ch>
2021-08-31 16:20:21 +0200

    adding pooling varaibles

--- masterfile.sh
+++ masterfile.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 #Masterfile for flagstat summary
-runID=screening_run_4.21
-in_path=/ibex_genomics/raw_data/ancient_raw_data/screening_rawdata/screening_run_4.21/Ibex/dedup
-out_path=/ibex_genomics/raw_data/ancient_raw_data/screening_rawdata/screening_run_4.21/Ibex/bam_statistics
-sample_file_path=/ibex_genomics/raw_data/ancient_raw_data/screening_rawdata/screening_run_4.21/Ibex/work/samples.txt
-base_name=_dedup
-MappingQuality=30
-Chrom=23
-REFGENOME=/ibex_genomics/raw_data/refgenom/GCF_001704415.1_ARS1_genomic.renamed.fna
+    runID=screening_run_4.21
+    in_path=/ibex_genomics/raw_data/ancient_raw_data/screening_rawdata/screening_run_4.21/Ibex/dedup
+    out_path=/ibex_genomics/raw_data/ancient_raw_data/screening_rawdata/screening_run_4.21/Ibex/bam_statistics
+    sample_file_path=/ibex_genomics/raw_data/ancient_raw_data/screening_rawdata/screening_run_4.21/Ibex/work/samples.txt 
+    base_name=_dedup
+#Variables for summary statistics
+    MappingQuality=30
+    Chrom=23
+    REFGENOME=/ibex_genomics/raw_data/refgenom/GCF_001704415.1_ARS1_genomic.renamed.fna
 
+#Variables for pooling scheme
+    #Number of lines which are use in the sequencing run
+    number_lanes=2
+    
+    #Number of ul of library, which should be added for each sample
+    ul_library_to_pool=5
+
+    #Final coverage to be achived over all runs
+    coverage_final=6
