04729cb1959456151dbbafe43b923bb55b75897a
Mathieu Robin <mathieu.robin@uzh.ch>
2021-09-01 15:03:22 +0200

    change master file

--- masterfile.sh
+++ masterfile.sh
@@ -7,15 +7,18 @@
     base_name=_dedup
 #Variables for summary statistics
     MappingQuality=30
-    Chrom=23
+    Chrom=1
     REFGENOME=/ibex_genomics/raw_data/refgenom/GCF_001704415.1_ARS1_genomic.renamed.fna
-
+    MaxOutputReads_Sequencer=3360.99 #In millions
 #Variables for pooling scheme
     #Number of lines which are use in the sequencing run
     number_lanes=2
-    
+
     #Number of ul of library, which should be added for each sample
     ul_library_to_pool=5
 
     #Final coverage to be achived over all runs
     coverage_final=6
+     
+    #Number of reads, the sequencer can produce 
+   
