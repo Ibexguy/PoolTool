566a5aff326b578d40b2c80632365136f7cde781
Ibexguy <mathieu.robin@uzh.ch>
2022-07-01 12:39:22 +0200

    chaning mastefile for whoel genome messurment of coverage

--- masterfile.sh
+++ masterfile.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #Masterfile for flagstat summary
-    runID=screening_run_4.21
-    in_path=/ibex_genomics/raw_data/ancient_raw_data/screening_rawdata/screening_run_4.21/Ibex/dedup
-    out_path=/ibex_genomics/raw_data/ancient_raw_data/screening_rawdata/screening_run_4.21/Ibex/bam_statistics
-    sample_file_path=/ibex_genomics/raw_data/ancient_raw_data/screening_rawdata/screening_run_4.21/Ibex/work/samples.txt 
-    base_name=.woNreads
+    runID=allSamples
+    in_path=/ibex_genomics/raw_data/ancient_raw_data/screening_rawdata/$runID/Ibex/dedup
+    out_path=/ibex_genomics/raw_data/ancient_raw_data/screening_rawdata/$runID/Ibex/bam_statistics
+    sample_file_path=/home/cluster/matrob/scratch/programs/Cluster_git/Cluster_Repo/Angsd-Pipelines/Sample_list/sample_list_extendedAllCaprines.txt
+    base_name=_recalibrated
 #Variables for summary statistics
     MappingQuality=30
     Chrom=1
     REFGENOME=/ibex_genomics/raw_data/refgenom/GCF_001704415.1_ARS1_genomic.renamed.fna
-    MaxOutputReads_Sequencer=3361 #In millions
+
 #Variables for pooling scheme
     #Number of lines which are use in the sequencing run
     number_lanes=2
@@ -21,4 +21,4 @@
     coverage_final=6
      
     #Number of reads, the sequencer can produce 
-   
+    MaxOutputReads_Sequencer=3361 #In millions
